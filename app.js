ee4d7d1da48ce3b5ed8a3eb098c9cf49ec031ce9
Irfan Ezani <irfanezani@gmail.com>
2020-06-11 12:33:26 +0800

    basic docs

---
+++ app.js
@@ -0,0 +1,3 @@
+const windVelocity = document.getElementById('w-velocity').value;
+const temperature = document.getElementById('temperature').value;
+const answer = document.getElementById('answer').value;
